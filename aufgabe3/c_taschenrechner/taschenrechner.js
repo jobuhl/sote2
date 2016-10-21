f8693d66986eb1fb308afe1f7ebc11fe1295d2d2
urskenner <urskenner@web.de>
2016-10-21 13:49:18 +0200

    upload

--- aufgabe3/c_taschenrechner/taschenrechner.js
+++ aufgabe3/c_taschenrechner/taschenrechner.js
@@ -1,3 +1,14 @@
 /**
  * Created by Jojo on 21.10.16.
  */
+function insertText(){
+
+    /*Holt den Wert der Anzeige*/
+    var wert = document.getElementById('td1').firstChild.nodeValue;
+    alert(wert);
+
+    /*Holt den Wert der Zahl*/
+    var eins = document.getElementById('b1').innerText;
+    alert(eins)
+
+}
